be737d46a2ee7630ee57ae83e9ae5bec5ebad47d
AhlemOuldAli <32915388+AhlemOuldAli@users.noreply.github.com>
2017-10-24 03:28:31 +0200

    Update main..java

--- main..java
+++ main..java
@@ -1,47 +1,66 @@
+
 package igl;
+
 import java.util.Scanner;
 
+/**
+ * <b> la classe main represente le programme principal </b>
+ *
+ */
 public class main {
 
+    /**
+     *la methode main qui permet d'utiliser les Fonctions et les methodes definies dans la classe VectorHelper
+     * et traiter l'exception SommeException si jamais il y a une anomalie.
+     * @param args non utilisé 
+     */
+   
+ 
     public static void main(String[] args) {
-        int A;
-       /*Lecture de la taille du vecteur*/ 
+
+         int A;  
+          
+         /*Lecture de la taille du vecteur*/
+          
        System.out.println("enter the size of the vector");
        Scanner s=new Scanner(System.in);
        int size=s.nextInt();
-       /*Création d'un objet de la classe vectorHelper*/
+       
+       /*la creation d'un objet de la classe VectorHelper*/
        VectorHelper objet=new VectorHelper();
-       /*Initialisation du vecteur*/
+       /*Initialissation du vecteur*/
        int []vector=objet.CreatVetor(size);
-       /*Afficher le vecteur*/
+       
        System.out.println("here is the vector");
+       /*Afficher le Vecteur crée*/
        objet.showVector(vector);
        
-       /*Le menu qui contient les opération que l'utilisateur peut effectuer*/
+       /*Le menu qui contient les operations que l'utilisateur peut effectuer*/
        
        System.out.println("choose the operation you want to apply to the vector \n "
-               + "1 for a sorting in a descending order \n 2 for a sorting in ascending order "
-               + "\n 3 to Add two vectors \n 4 to inverse the vector \n 5 to show the MinMax element \n"
-               + " 6 to test parity of elements. ");
+               + "* 1 for a sorting in a descending order \n *2 for a sorting in ascending order "
+               + "\n *3 to Add two vectors \n *4 to inverse the vector \n *5 to show the MinMax element \n"
+               + " *6 to test parity of elements. ");
+       
        Scanner sa=new Scanner(System.in);
        A=sa.nextInt();
-
+       
        switch (A){
-               /*1:trier le vecteur d'une façon decsendante*/
+                    /*1:trier le vecteur d'une façon descendante*/
                case 1:objet.decSortVector(vector);
-               break;
-                     
-                /* 2:trier le vecteur d'une façon croissante*/   
+                     break;
+                   /*2:trier le vecteur d'une façon croissante*/
                case 2:objet.CrSortVector(vector);
-               break;
-                   
-                   /*Sommer deux vecteur*/
+                   break;
+                   /*3:sommer deux vecteurs*/
                case 3:{
                    System.out.println("enter the size of the second vector");
                    Scanner s2=new Scanner(System.in);
+                   
                    int size1=s2.nextInt();
                    int [] vector1=new int[size1];
-                    /*bloc contenant des instructions qui peut generer une Exception*/
+                   
+                   /*bloc contenant des instructions qui peut generer une Exception*/
                    try{
                        objet.AddVector(vector,size1);
                    }
@@ -50,25 +69,24 @@ public class main {
                        System.err.println(E);
                        
                    }
-                   
-       
+           
                    }
                break;
-                   /*4:inverser le vecteur*/
-               case 4:objet.reverseVector(vector);
-               break;
-                   
-                   /*5:Afficher le min et le max*/
-               case 5:objet.minMaxVector(vector);
-               break;
+                   /*inverser un vecteur*/
                   
-                   /*6:Tester la paeité des éléments*/
+               case 4:objet.reverseVector(vector);
+                   break;
+                   /*afficher le min et le max d'un vecteur*/
+               case 5:objet.minMaxVector(vector);
+                   break;
+                   /*tester le parité des elements d'un vecteur*/
                case 6:objet.parityVector(vector);
-               break;
-                   
+                   break;
                           
        }
                
            
        }
     }
+    
+
